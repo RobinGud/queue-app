64deeb228f0fb32d92ccbe3efeb3038ee08fa957
Nikita Matsnev <niki-sim@mail.ru>
2021-11-15 14:40:57 +0300

    add fonts

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    GilroyBold: require('./assets/fonts/Gilroy-Bold.ttf'),
+    GilroyExtrabold: require('./assets/fonts/Gilroy-Extrabold.ttf'),
+    GilroyLight: require('./assets/fonts/Gilroy-Light.ttf'),
+    IBMPlexSans: require('./assets/fonts/IBMPlexSans.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'GilroyBold', fontSize: 30}}>Open up App.js to start working on your app! HELLO</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +29,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
