cf7a776823d142be2e89d7360420822db1960158
Nikita Matsnev <niki-sim@mail.ru>
2021-11-15 14:03:52 +0300

    add main colors

---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+export default {
+    white: '#FFFFFF',
+    black: '#000000',
+    blue: '#001AFF',
+    darkGrey1: '#333333',
+    darkGrey2: '#4F4F4F',
+    grey1: '#828282',
+    grey2: '#BDBDBD',
+    red: '#EB5757',
+    orange: '#F2994A',
+    green: '#219653'
+};
